bf4b3277e93b5bb9346653b031187adb95a12159
Jose Rodriguez <boriel@gmail.com>
2021-10-03 23:08:12 +0200

    test: add test case for no headers

--- tests/test_lightcsv.py
+++ tests/test_lightcsv.py
@@ -23,6 +23,32 @@ class TestLightCSV(unittest.TestCase):
             "C6": datetime.time(23, 50, 12)
         }]
 
+    def test_it_parses_with_no_column(self):
+        input = ('C0,C1,C2,C3,C4,C5,C6\n'
+                 ',1,2.5,"a ""String""",2021-02-05,2021-02-05 23:50:12,23:50:12\n')
+        with StringIO(input) as f:
+            result = list(LightCSV(strict=False).read(f))
+
+        assert result == [
+            {
+                0: "C0",
+                1: "C1",
+                2: "C2",
+                3: "C3",
+                4: "C4",
+                5: "C5",
+                6: "C6"
+            },
+            {
+                0: None,
+                1: 1,
+                2: 2.5,
+                3: 'a "String"',
+                4: datetime.date(2021, 2, 5),
+                5: datetime.datetime(2021, 2, 5, 23, 50, 12),
+                6: datetime.time(23, 50, 12)
+            }]
+
 
 if __name__ == '__main__':
     unittest.main()
